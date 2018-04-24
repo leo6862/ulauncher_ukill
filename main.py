5ad9387977596c583a55779c67722e5514794f03
İbrahim Serdar Açıkgöz <serdaracikgoz86@gmail.com>
2018-04-24 17:32:58 +0300

    prettified a litte, but there is a lot of thing to do.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from subprocess import Popen, PIPE, check_call, CalledProcessError
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.item.ExtensionSmallResultItem import ExtensionSmallResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
@@ -44,20 +45,23 @@ class KeywordQueryEventListener(EventListener):
         return RenderResultListAction(list(islice(self.generate_results(event), 15)))
 
     def generate_results(self, event):
-        for (pid, cpu, cmd) in get_process_list():
-            name = '[%s%% CPU] %s' % (cpu, cmd) if cpu > 1 else cmd
+        for (pid, cpu, mem, cmd, cmd_long) in get_process_list():
+            name = ('%s %s' % (cmd, pid))
+            description = ('cpu %% : %s mem %% : %s loc : %s ' % (cpu, mem, cmd_long))
             on_enter = {'alt_enter': False, 'pid': pid, 'cmd': cmd}
             on_alt_enter = on_enter.copy()
             on_alt_enter['alt_enter'] = True
             if event.get_argument():
                 if event.get_argument() in cmd:
-                    yield ExtensionSmallResultItem(icon=exec_icon,
+                    yield ExtensionResultItem(icon=exec_icon,
                                                    name=name,
+                                                   description=description,
                                                    on_enter=ExtensionCustomAction(on_enter),
                                                    on_alt_enter=ExtensionCustomAction(on_alt_enter, keep_app_open=True))
             else:
-                yield ExtensionSmallResultItem(icon=exec_icon,
+                yield ExtensionResultItem(icon=exec_icon,
                                                name=name,
+                                               description=description,
                                                on_enter=ExtensionCustomAction(on_enter),
                                                on_alt_enter=ExtensionCustomAction(on_alt_enter, keep_app_open=True))
 
@@ -85,8 +89,9 @@ class ItemEnterEventListener(EventListener):
             on_enter = data.copy()
             on_enter['alt_enter'] = False
             on_enter['signal'] = sig
-            result_items.append(ExtensionSmallResultItem(icon=ext_icon,
+            result_items.append(ExtensionResultItem(icon=ext_icon,
                                                          name=name,
+                                                         description=description,
                                                          highlightable=False,
                                                          on_enter=ExtensionCustomAction(on_enter)))
         return RenderResultListAction(result_items)
@@ -101,13 +106,15 @@ class ItemEnterEventListener(EventListener):
 
 def get_process_list():
     """
-    Returns a list of tuples (PID, %CPU, COMMAND)
+    Returns a list of tuples (PID, CPU, MEM, COMMAND, LONG_COMMAND)
     """
     env = os.environ.copy()
     env['COLUMNS'] = '200'
-    process = Popen(['top', '-bn1', '-cu', os.getenv('USER')], stdout=PIPE, env=env)
+    process = Popen(['top', '-bn1', '-u', os.getenv('USER')], stdout=PIPE, env=env)
+    process_lc = Popen(['top', '-bn1', '-cu', os.getenv('USER')], stdout=PIPE, env=env)
     out = process.communicate()[0]
-    for line in out.split('\n'):
+    lc_output = process_lc.communicate()[0].split('\n')
+    for idx, line in enumerate(out.split('\n')):
         col = line.split()
         try:
             int(col[0])
@@ -117,11 +124,16 @@ def get_process_list():
 
         pid = col[0]
         cpu = atof(col[8])
+        mem = atof(col[9])
         cmd = ' '.join(col[11:])
+        try:
+            cmd_long = lc_output[idx].split()[11]
+        except (ValueError, IndexError):
+            cmd_long = ""
         if 'top -bn' in cmd:
             continue
 
-        yield (pid, cpu, cmd)
+        yield (pid, cpu, mem, cmd, cmd_long)
 
 
 if __name__ == '__main__':
